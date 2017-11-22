e7480aa9920ababff7a6829a446b41324291a10c
akihumii <a0126587@u.nus.edu>
2017-11-22 18:35:54 +0800

    try to add endpoint to other spike detection methods... not successful yet lol

---
+++ PostProcessing/getPointsWithinRange.m
@@ -0,0 +1,24 @@
+function output = getPointsWithinRange(data,startLocs,endLocs)
+%getPointsWithinRange Get the points within the range
+%   Detailed explanation goes here
+
+[numRow,numColumn] = size(data);
+
+for i = 1:numColumn
+    numLocs = length(~isnan(startLocs(:,i)));
+    for j = 1:numLocs
+        burst{j,1} = data(transpose(startLocs(j,i):endLocs(j,i)));
+        xAxisValues{j,1} = transpose(1:(endLocs(j,i)-startLocs(j,i)+1)); % create an array of x axis values for burst plotting
+    end
+    burstAll{i,1} = cell2nanMat(burst);
+    xAxisValuesAll{i,1} = cell2nanMat(xAxisValues);
+end
+
+burstAll = cell2nanMat(burstAll);
+xAxisValuesAll = cell2nanMat(xAxisValuesAll);
+
+output.burst = burstAll;
+output.xAxisValues = xAxisValuesAll;
+output.numBursts = num
+end
+
