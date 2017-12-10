d9492557297ce71b5ea5ddae2feac3ad84ad6259
akihumii <a0126587@u.nus.edu>
2017-12-11 04:00:06 +0800

    added option to choose different days and same speed as condition !.!

--- analyzeFeatures.m
+++ analyzeFeatures.m
@@ -7,7 +7,7 @@ clc
 
 %% User Input
 showFigures = 1;
-saveFigures = 0;
+saveFigures = 1;
 
 [files, path, iter] = selectFiles();
 
@@ -19,7 +19,8 @@ for i = 1:iter
     
     fileName{i,1} = signal(i,1).fileName;
     features{i,1} = signalClassification(i,1).features;
-    speed(i,1) = str2double(fileName{i,1}(7:8));
+    fileSpeed{i,1} = fileName{i,1}(7:8);
+    fileDate{i,1} = fileName{i,1}(12:17);
 end
 
 channel = signal(1,1).channel;
@@ -48,21 +49,32 @@ close all
 
 dataPath = signal(1,1).path;
 titleName = 'Different Speed';
-plotFileName = [fileName{1,1}(1:6),fileName{1,1}(12:17)];
-xScale = 'Speed(cm/s)';
+switch titleName
+    case 'Different Speed'
+        plotFileName = [fileName{1,1}(1:6),fileName{1,1}(12:17)];
+        xScale = 'Speed(cm/s)';
+        xTickValue = fileSpeed;
+    case 'Different Day';
+        plotFileName = fileName{1,1}(1:8);
+        xScale = 'Date';
+        xTickValue = fileDate;
+end
+        
 
 for i = 1:numFeatures
     for j = 1:numChannel
-        [p(i,j),f(i,j)] = plotFig(speed,transpose(featureMean(i,:,j)),plotFileName,[featuresNames{i,1},' of ',titleName],xScale,'',...
+        [p(i,j),f(i,j)] = plotFig(1:iter,transpose(featureMean(i,:,j)),plotFileName,[featuresNames{i,1},' of ',titleName],xScale,'',...
             0,... % save
             1,... % show
             path, 'subPlot',channel(1,j),'barPlot');
+        p(i,j).XTick = 1:iter;
+        p(i,j).XTickLabel = xTickValue;
         hold on
         featureStde(i,:,j) = featureStd(i,:,j) / sqrt(numBursts);
-        errorbar(speed,transpose(featureMean(i,:,j)),featureStde(i,:,j),'r*');
+        errorbar(1:iter,transpose(featureMean(i,:,j)),featureStde(i,:,j),'r*');
         
         if saveFigures
-            savePlot(dataPath,['Features sorted in ',titleName,],plotFileName,[featuresNames{i,1},' with ',titleName,' of ch ',num2str(channel(1,j)),' in ',plotFileName])
+            savePlot(path,['Features sorted in ',titleName,],plotFileName,[featuresNames{i,1},' with ',titleName,' of ch ',num2str(channel(1,j)),' in ',plotFileName])
         end
         
     end
@@ -74,7 +86,7 @@ for i = 1:numChannel
 end
 
 if saveFigures
-    savePlot(dataPath,['Features sorted in ',titleName,],plotFileName,['All the Features with ',titleName,' of ch ',num2str(channel(1,j)),' in ',plotFileName])
+    savePlot(path,['Features sorted in ',titleName,],plotFileName,['All the Features with ',titleName,' of ch ',num2str(channel(1,j)),' in ',plotFileName])
 end
 
 if ~showFigures
