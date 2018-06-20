d0d6ca93356fecfc15a67168a352f1cada94bfad
akihumii <a0126587@u.nus.edu>
2018-06-20 11:56:58 +0800

    made it compatible for testing the classifier for multiple times, but realized that the classifier model is always the same, so it is meaningless to test the same classifier model for so many times :(

--- Classification/runPrediction.m
+++ Classification/runPrediction.m
@@ -4,49 +4,64 @@ function output = runPrediction(classifierOutput,threshPercentile)
 %   output = runClassifier(classifierOutput,pcaInfo)
 
 %% Parameters
-windowSize = 0.5; % window size in seconds
-windowSkipSize = 0.05; % skipped window size in seconds
-correctClass = 1; % real class of the signal bursts
+numRepeatsTesting = 1000;
 
 %% Read files
 [filesTest,pathTest,iterTest] = selectFiles('select mat files for testing classifier');
 
 %% Run classifier
-popMsg('Processing continuous classification...');
+popMsg('Processing classifier testing...');
+tTesting = tic;
 
-for i = 1:iterTest % test the classifier
-    testSignalInfo(i,1) = getFeaturesInfo(pathTest,filesTest{i,1});
+% initiate required variables
+numFeatureSet = length(classifierOutput.classificationOutput); % number of feature dimensions used for classification, eg numFeatureSet = 2 means that there is up to 2-D of classification
+numChannel = length(classifierOutput.classificationOutput{1,1}.Mdl);
 
-    testPCAInfo(i,1) = reconstructPCA(testSignalInfo,threshPercentile); % matrix in [class x channel]
+groupedFeature = cell(numFeatureSet,numChannel,iterTest);
+predictClass = cell(numFeatureSet,numChannel,iterTest);
+accuracyTemp = cell(numFeatureSet,numChannel,iterTest);
 
-    testFeaturesRaw{i,1} = reconstructSignalInfoFeatures(testSignalInfo(i,1));
-    
-    featuresInfo{i,1} = reconstructFeatures(testFeaturesRaw{i,1},1,testPCAInfo(i,1).numBursts); % as the raw features still contains Nan, so number of bursts should not be trimmed too
-    
-
-    switch testSignalInfo(i,1).fileSpeed{1,1}
-        case num2str(10)
-            classTemp = 1;
-        case num2str(15)
-            classTemp = 2;
-        otherwise
-            error('Invalid input class...')
+for n = 1:numRepeatsTesting
+    for i = 1:iterTest % test the classifier
+        testSignalInfo(i,1) = getFeaturesInfo(pathTest,filesTest{1,i});
+        
+        testPCAInfo(i,1) = reconstructPCA(testSignalInfo(i,1),threshPercentile); % matrix in [class x channel]
+        
+        testFeaturesRaw{i,1} = reconstructSignalInfoFeatures(testSignalInfo(i,1));
+        
+        featuresInfo{i,1} = reconstructFeatures(testFeaturesRaw{i,1},1,testPCAInfo(i,1).numBursts); % as the raw features still contains Nan, so number of bursts should not be trimmed too
+        
+        % assign the number in the filename into certain classes
+        switch testSignalInfo(i,1).fileSpeed{1,1}
+            case num2str(10)
+                classTemp = 1;
+            case num2str(15)
+                classTemp = 2;
+            otherwise
+                error('Invalid input class...')
+        end
+        
+        for j = 1:numFeatureSet
+            for k = 1:numChannel
+                trainingRatio = 0;
+                groupedFeature{j,k,i}(:,n) = combineFeatureWithoutNan(featuresInfo{i,1}.featuresAll(:,classifierOutput.featureIndex{j,1}(1,:),k),trainingRatio,1);
+                
+                predictClass{j,k,i}(:,n) = predict(classifierOutput.classificationOutput{j,1}.Mdl{k},groupedFeature{j,k,i}(:,end).testing); % get the prediction
+                
+                accuracyTemp{j,k,i}(n,:) = calculateAccuracy(predictClass{j,k,i}(:,end),ones(size(predictClass{j,k,i}(:,end)))*classTemp);
+            end
+        end
+        %     predictionOutput(i,1) = discreteClassification(testSignalInfo(i,1).dataTKEOTest,testSignalInfo(i,1).dataFilteredTest,testSignalInfo(i,1).samplingFreqTest,windowSize,windowSkipSize,testSignalInfo(i,1).detectionInfoTest,classifierOutput.featureIndex,classifierOutput.coefficient,correctClass);
     end
-    
-    numFeatureSet = length(classifierOutput.classificationOutput); % number of feature dimensions used for classification, eg numFeatureSet = 2 means that there is up to 2-D of classification
-    numChannel = length(classifierOutput.classificationOutput{1,1}.Mdl);
+end
+
+% Compute the average
+for i = 1:iterTest
     for j = 1:numFeatureSet
         for k = 1:numChannel
-            trainingRatio = 0;
-            groupedFeature(j,k,i) = combineFeatureWithoutNan(featuresInfo{i,1}.featuresAll(:,classifierOutput.featureIndex{j,1}(1,:),k),trainingRatio,iterTest);
-            
-            predictClass{j,k,i} = predict(classifierOutput.classificationOutput{j,1}.Mdl{k},groupedFeature(j,k,i).testing); % get the prediction
-            
-            accuracyTemp(j,k,i) = calculateAccuracy(predictClass{j,k,i},ones(size(predictClass{j,k}))*classTemp);
+            resultedAccuracy(j,k,i) = getBasicParameter(vertcat(accuracyTemp{j,k,i}.accuracy));
         end
     end
-    
-%     predictionOutput(i,1) = discreteClassification(testSignalInfo(i,1).dataTKEOTest,testSignalInfo(i,1).dataFilteredTest,testSignalInfo(i,1).samplingFreqTest,windowSize,windowSkipSize,testSignalInfo(i,1).detectionInfoTest,classifierOutput.featureIndex,classifierOutput.coefficient,correctClass);
 end
 
 % for i = 1:iterTest % visualize the classifier
@@ -56,9 +71,11 @@ end
 % display(['Continuous classification takes ',num2str(toc(tTrain)),' seconds...']);
 
 %% Output
+output.resultedAccuracy = resultedAccuracy;
 output.prediction = accuracyTemp;
 output.predictClass = predictClass;
 output.signalInfo = testSignalInfo;
+output.iterTest = iterTest;
 
 end
 
