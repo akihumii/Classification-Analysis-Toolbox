5a333076c07e65f1dcb72f8152c3910364485706
akihumii <a0126587@u.nus.edu>
2018-07-16 02:09:44 +0800

    changed features used in classier training session and also added a function to store the useful information of the classification such as the accuracy and the feature index that yields the highest accuracy

---
+++ PostProcessing/saveClassificationVariables.m
@@ -0,0 +1,29 @@
+function [] = extractFeatures()
+%EXTRACTFEATURES Extract the features and save it in a new mat file.
+%   Detailed explanation goes here
+
+allMat = dir('*.mat');
+numMat = length(allMat);
+
+for i = 1:numMat
+    summaryMat(i,1).name = allMat.name;
+    matTemp = load(allMat(i,1).name);
+    summaryMat(i,1).accuracy = matTemp.varargin{1,1}.accuracyMax;
+    summaryMat(i,1).maxFeatureCombo = matTemp.varargin{1,1}.maxFeatureCombo;
+    
+    name{i,1} = summaryMat(i,1).name;
+    oneDimTA(i,1) = summaryMat(i,1).accuracy(1,1);
+    oneDimGC(i,1) = summaryMat(i,1).accuracy(1,2);
+    twoDimTA(i,1) = summaryMat(i,1).accuracy(2,1);
+    twoDimGC(i,1) = summaryMat(i,1).accuracy(2,2);
+    
+    oneDimFeature(i,1) = summaryMat(i,1).maxFeatureCombo{1,1};
+    twoDimFeature(i,:) = summaryMat(i,1).maxFeatureCombo{2,1};
+end
+
+summaryTable = table(name,oneDimTA,oneDimGC,twoDimTA,twoDimGC,oneDimFeature,twoDimFeature);
+
+writetable(summaryTable,['summaryTable',time2string]);
+
+end
+
