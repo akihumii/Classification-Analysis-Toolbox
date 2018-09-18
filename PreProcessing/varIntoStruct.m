cc1635ca353b512e83bfa7843c1ea69651932b16
akihumii <a0126587@u.nus.edu>
2018-09-18 11:33:43 +0800

    fixed a bug in the varIntStruct so that it can pump the input parameters into default parameters properlly :|

--- PreProcessing/varIntoStruct.m
+++ PreProcessing/varIntoStruct.m
@@ -5,7 +5,7 @@ function output = varIntoStruct(structure,varargin)
 output = structure;
 
 if ~isempty(varargin{1,1})
-    for i = 1:2:nargin-1
+    for i = 1:2:length(varargin{1,1})-1
         if ismember(varargin{1,1}{1,i},fieldnames(structure))
             output.(varargin{1,1}{1,i}) = varargin{1,1}{1,i+1};
         end
