72c8cafb2a20cf4a71f6a4883656067b2b244411
akihumii <a0126587@u.nus.edu>
2018-10-09 12:08:23 +0800

    changed popMsg.m so that the counting for closing the window will run in background ;D

--- General/popMsg.m
+++ General/popMsg.m
@@ -1,12 +1,32 @@
 function box = popMsg(content)
 %popMsg Pop the message box to show the popping content
-%   box = popMsg(content)
+%   [] = popMsg(content)
 
-box = msgbox(content);
-pause(2)
-delete(box)
-disp(content)
-disp(' ')
+timeClose = 10; % auto close the window once this timing has passed
+t = timer;
+set(t,'StartFcn', {@startTimerFcn, content});
+set(t,'TimerFcn', {@runTimerFcn, content});
+set(t,'StartDelay', timeClose);
+set(t,'StopFcn', {@stopTimerFcn, t});
 
+start(t)
+box = t.UserData;
 end
 
+function startTimerFcn(obj,~,content)
+box = msgbox(content);
+set(obj,'UserData',box);
+end
+
+function runTimerFcn(~,~,content)
+disp(content)
+end
+
+function stopTimerFcn(obj,~,t)
+box = obj.UserData;
+if exist('box', 'var')
+    delete(box);
+    clear('box');
+end
+delete(t)
+end
