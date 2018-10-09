0eeb6920e11fd1bd8b37d4759703a220a4bcb3b4
akihumii <a0126587@u.nus.edu>
2018-10-09 12:26:49 +0800

    it'll now delete all the boxes before generating another new box window XD

--- General/popMsg.m
+++ General/popMsg.m
@@ -1,16 +1,19 @@
 function box = popMsg(content)
 %popMsg Pop the message box to show the popping content
 %   [] = popMsg(content)
+warning('off','all')
+delete(timerfind('Tag','box'))
 
-timeClose = 10; % auto close the window once this timing has passed
-t = timer;
-set(t,'StartFcn', {@startTimerFcn, content});
-set(t,'TimerFcn', {@runTimerFcn, content});
-set(t,'StartDelay', timeClose);
-set(t,'StopFcn', {@stopTimerFcn, t});
+timeClose = 5; % auto close the window once this timing has passed
+boxT = timer;
+set(boxT,'StartFcn', {@startTimerFcn, content});
+set(boxT,'TimerFcn', {@runTimerFcn, content});
+set(boxT,'StartDelay', timeClose);
+set(boxT,'StopFcn', @stopTimerFcn);
+set(boxT,'Tag','box');
 
-start(t)
-box = t.UserData;
+start(boxT)
+box = boxT.UserData;
 end
 
 function startTimerFcn(obj,~,content)
@@ -22,11 +25,14 @@ function runTimerFcn(~,~,content)
 disp(content)
 end
 
-function stopTimerFcn(obj,~,t)
-box = obj.UserData;
-if exist('box', 'var')
-    delete(box);
-    clear('box');
+function stopTimerFcn(obj,~)
+try
+    box = obj.UserData;
+    if exist('box', 'var')
+        delete(box);
+        clear('box');
+    end
+    delete(obj)
+catch
 end
-delete(t)
 end
