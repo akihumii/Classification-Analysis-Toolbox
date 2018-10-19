49f56c3172b667075eac5f152598b4f1d8b2b85e
akihumii <a0126587@u.nus.edu>
2018-10-19 18:34:03 +0800

    for the signal that doesn't have noise or weird bursts :o

---
+++ PostProcessing/getBaselineFeature.m
@@ -0,0 +1,11 @@
+function output = getBaselineFeature(data,input)
+%GETBASELINEFEATURE Get the features for baseline, the bursts in this case
+%is the chunks that are not detected as bursts, which is the part starting
+%from the burst offset to the next burst's onset.
+% 
+%   [] = getBaselineFeature(data,input)
+
+
+
+end
+
