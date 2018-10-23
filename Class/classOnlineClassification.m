21c1ea5bbdce9013263e69a55e930a9bdaeeb0ae
shih91at <shih91at@gmail.com>
2018-10-23 15:18:13 +0800

    phase one complete -- by Shih Chiang ;)

--- Class/classOnlineClassification.m
+++ Class/classOnlineClassification.m
@@ -1,9 +1,9 @@
 classdef classOnlineClassification < matlab.System
-    % For online classifiation 
+    % For online classifiation
     %
     % This template includes the minimum set of functions required
     % to define a System object with discrete state.
-
+    
     % Public, tunable properties
     properties
         predictClass = 0;
@@ -11,6 +11,7 @@ classdef classOnlineClassification < matlab.System
         numOnsetBurst
         numOffsetBurst
         windowSize
+        overlapWindowSize
         samplingFreq
         host
         port
@@ -22,34 +23,39 @@ classdef classOnlineClassification < matlab.System
         featureClassification; % corresponds to mean value
         classifierMdl
         numClass
-%         dataTKEO
-%         dataFiltered
-        windowFull = 0;
-        readyDetect = 0;
+        %         dataTKEO
+        %         dataFiltered
         readyClassify = 0;
     end
-
+    
     properties(DiscreteState)
-
+        
     end
-
+    
     % Pre-computed constants
     properties(Access = private)
         lengthTKEO = 13;
         numChannel = 0;
         t
+        stepRead
+        startOverlapping = 0;
     end
-
+    
     methods
-        function setBasicParameters(obj,data)
+        function setBasicParameters(obj,data,parameters)
             obj.threshold = data.thresholds;
             obj.numOnsetBurst = data.numStartConsecutivePoints;
             obj.numOffsetBurst = data.numEndConsecutivePoints;
             obj.samplingFreq = data.samplingFreq;
-            obj.windowSize = setupMaxBurstLength(obj);
+            %             obj.windowSize = setupMaxBurstLength(obj);
+            obj.windowSize = 50;
             obj.featureClassification = data.featureClassification;
             obj.classifierMdl = data.classifierMdl;
             obj.numClass = data.numClass; % including one class of resting state
+            
+            obj.overlapWindowSize = parameters.overlapWindowSize;
+            
+            updateStepRead(obj); % update some constants
         end
         
         function setTcpip(obj,host,port,varargin)
@@ -61,8 +67,8 @@ classdef classOnlineClassification < matlab.System
         
         function setInitialData(obj) % initialize data
             obj.dataRaw = cell(obj.numChannel,1);
-%             obj.dataTKEO = cell(obj.numChannel,1);
-%             obj.dataFiltered = cell(obj.numChannel,1);
+            %             obj.dataTKEO = cell(obj.numChannel,1);
+            %             obj.dataFiltered = cell(obj.numChannel,1);
         end
         
         function tcpip(obj)
@@ -73,44 +79,44 @@ classdef classOnlineClassification < matlab.System
         
         function openPort(obj)
             for i = 1:obj.numChannel
-                fopen(obj.t(i,1));
-                disp(['Open port ',num2str(ports),'...']);
+                fopen(obj.t{i,1});
+                disp(['Open port ',num2str(obj.port),'...']);
             end
         end
         
         function closePort(obj)
             for i = 1:obj.numChannel
-                fclose(obj.t(i,1));
-                disp(['Close port ',num2str(ports),'...']);
+                fclose(obj.t{i,1});
+                disp(['Close port ',num2str(obj.port),'...']);
             end
         end
         
         function readSample(obj)
-            lengthData = length(obj.dataRaw{1,1});
-            obj.readyDetect = lengthData > obj.lengthTKEO; % activate ready detect
-            obj.windowFull = lengthData > obj.windowSize; % activate trimming of samples
-            
             for i = 1:obj.numChannel
-                sample = fread(obj.t{i,1}, obj.t{i,1}.BytesAvailable);
-                
-                obj.dataRaw{1,1} = [obj.dataRaw{1,1}; sample]; % accumulate samples at the beginning
-                
-                if obj.windowFull
-                    obj.dataRaw{1,1}(1,1) = []; % remove the excess samples 
+                if ~obj.startOverlapping
+                    for j = 1:obj.stepRead:obj.windowSize
+                        obj.dataRaw{i,1} = [obj.dataRaw{i,1} ; fread(obj.t{i,1}, obj.stepRead, 'double')];
+                    end
+                else
+                    for j = 1:obj.stepRead:obj.overlapWindowSize
+                        sample = fread(obj.t{i,1}, obj.stepRead, 'double');
+                    obj.dataRaw{i,1} = fixWindow(obj,obj.dataRaw{i,1},sample);
+                    end
                 end
+                
+                obj.dataRaw{i,1}
             end
+            obj.startOverlapping = 1;
         end
         
         function detectBurst(obj)
-            if obj.readyDetect
-                for i = 1:obj.numChannel
-                    dataTKEO = TKEO(obj.dataRaw{i,1},obj.samplingFreq);
-                    [peaks,~] = triggerSpikeDetection(dataTKEO,obj.threshold(1,i),0,obj.numOnsetBurst(1,i),0);
-                    if ~isnan(peaks)
-                        obj.readyClassify = 1; % activate flag for classify
-                        break % break the for loop whenever a burst is found
-                    end
-                end                               
+            for i = 1:obj.numChannel
+                dataTKEO = TKEO(obj.dataRaw{i,1},obj.samplingFreq);
+                [peaks,~] = triggerSpikeDetection(dataTKEO,obj.threshold(1,i),0,obj.numOnsetBurst(1,i),0);
+                if ~isnan(peaks)
+                    obj.readyClassify = 1 % activate flag for classify
+                    break % break the for loop whenever a burst is found
+                end
             end
         end
         
@@ -119,17 +125,19 @@ classdef classOnlineClassification < matlab.System
                 features = cell(obj.numChannel,1);
                 for i = 1:obj.numChannel
                     dataFiltered = filter(obj,i); % get the filtered data for each channel
-                    features{i,1} = featureExtraction(dataFiltered,obj.samplingFreq);
+                    featuresTemp = featureExtraction(dataFiltered,obj.samplingFreq);
+                    features(1,i) = featuresTemp.areaUnderCurve;
                 end
-                features = cell2nanMat(features);
                 obj.predictClass = predict(obj.classifierMdl, features);
+                
+                obj.readyClassify = 0 % to deactivate the readyClassify after the prediction
             end
         end
     end
     
     
     
-    methods(Access = protected)        
+    methods(Access = protected)
         function output = setupMaxBurstLength(obj)
             output = max([obj.numOnsetBurst(:); obj.numOffsetBurst(:)]);
         end
@@ -140,6 +148,19 @@ classdef classOnlineClassification < matlab.System
         
         function dataFiltered = filter(obj,i)
             dataFiltered = filterData(obj.dataRaw{i,1},obj.samplingFreq,obj.highPassCutoffFreq,obj.lowPassCutoffFreq,obj.notchFreq);
+        end
+        
+        function output = fixWindow(obj,dataRaw,sample)
+            output = [dataRaw(obj.stepRead+1 : end, 1) ; sample];
+        end
+        
+        function updateStepRead(obj)
+            for i = 50:-10:1
+                if ~mod(obj.windowSize,i)
+                    break
+                end
+            end
+            obj.stepRead = i;
         end
     end
 end
--- onlineClassification.m
+++ onlineClassification.m
@@ -2,32 +2,43 @@ function [] = onlineClassification()
 %ONLINECLASSIFIER Do the online classification in the Qt after reading the
 %prameters saved by onlineClassifierTraining
 %   Detailed explanation goes here
+% 
+% [files,path] = selectFiles('Select trained parameters...');
+% 
+% classifierParameters = load(fullfile(path,files{1,1}));
 
-[files,path] = selectFiles('Select trained parameters...');
+warning('off','all');
 
-classifierParameters = load(fullfile(path,files{1,1}));
+%% Parameters
+parameters = struct(...
+    'overlapWindowSize',50);
+
+classifierParameters = load('C:\Users\lsilsc\Desktop\OnlineClassificationInfo_20181023113558_2.mat');
 classifierParameters = classifierParameters.varargin{1,1};
 
 %% Initialization
-ports = [1340,1341];
+ports = [1345,1346];
 
 classInfo = classOnlineClassification();
 
-setBasicParameters(classInfo,classifierParameters);
+setBasicParameters(classInfo,classifierParameters,parameters);
 setTcpip(classInfo,'127.0.0.1',ports,'NetworkRole','client');
 setInitialData(classInfo);
 
 %% Streaming data
 tcpip(classInfo);
-openPort(obj);
+openPort(classInfo);
 
+i=1;
 while(1)
     readSample(classInfo);
+%     t = tic;
     detectBurst(classInfo);
     classifyBurst(classInfo);
+%     toc(t)
 end
 
-closePort(obj);
+closePort(classInfo);
 
 end
 
