ac5fa2de6567ef7ae73987d63b3f705ad13777da
akihumii <a0126587@u.nus.edu>
2018-12-27 13:45:56 +0800

    python scripts initiated :0

---
+++ onlineClassificationOnboard.py
@@ -0,0 +1,24 @@
+class Parameters:
+    def __init__(self):
+        self.trained_mdl =
+
+
+class GetWindow:
+    def __init__(self):
+        self.window_size = 100
+        self.window_size_overlap = 50
+
+    def read_sample(self):
+        self.data =
+
+
+class Filtering:
+    def __init__(self):
+        self.high_pass_threshold = 50
+        self.low_pass_threshold = 3500
+        self.notch_freq = 50
+
+
+if __name__ == "__main__":
+    a = "hi"
+    print a
