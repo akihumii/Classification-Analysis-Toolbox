daf5d6ecbbd148639582cfd89dec9043c122e743
akihumii <a0126587@u.nus.edu>
2019-01-08 11:19:03 +0800

    it can now save but there are data drop :(

--- onlineClassificationOnboard.py
+++ onlineClassificationOnboard.py
@@ -1,11 +1,12 @@
 import codecs
+from scipy import signal, zeros, random
 from subprocess import call
 import os
 import datetime
-import csv
 import socket
 import numpy as np
 from time import sleep
+
 
 # class Parameters(object):
 #     def __init__(self):
@@ -21,25 +22,41 @@ from time import sleep
 #         # self.data =
 #
 #
-# class Filtering:
-#     def __init__(self):
-#         self.high_pass_threshold = 50
-#         self.low_pass_threshold = 3500
-#         self.notch_freq = 50
+
+
+class Filtering:
+    def __init__(self, hp_thresh, lp_thresh, notch_thresh):
+        self.data_filtered = []
+
+        self.high_pass_threshold = 1. / hp_thresh
+        self.low_pass_threshold = 1. / lp_thresh
+        self.notch_freq = notch_thresh
+
+        self.filter_obj = None
+        self.filter_low_pass = None
+        self.filter_z = None  # initial condition of the filter
+        self.z_low_pass = None
+        self.__num_taps = 150
+
+        self.set_filter()
+
+    def set_filter(self):
+        self.filter_obj = signal.firwin(self.__num_taps,
+                                        [self.low_pass_threshold, self.high_pass_threshold], pass_zero=False)
+        self.filter_z = signal.lfilter_zi(self.filter_obj, 1)
+
+    def filter(self, data_orig):
+        self.data_filtered = [signal.lfilter(self.filter_obj, 1, [x], zi=self.filter_z) for x in data_orig]
+        # self.data_filtered = signal.lfilter(self.filter_obj, 1, data_orig, zi=self.filter_z)
 
 
 class TcpIp:
-    def __init__(self):
-        self.ip_add = ""
-        self.port = ""
-        self.buffer_size = 0
-        self.socket_obj = None
-        self.__connected = False
-
-    def set(self, ip_add, port, buffer_size):  # setup port
+    def __init__(self, ip_add, port, buffer_size):
         self.ip_add = ip_add
         self.port = port
         self.buffer_size = buffer_size
+        self.socket_obj = None
+        self.__connected = False
 
     def connect(self):  # connect to port
         self.socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -57,27 +74,27 @@ class TcpIp:
         print("Successfully connected...")
 
     def read(self):  # read data from port
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < self.buffer_size:
-            chunk = self.socket_obj.recv(min(self.buffer_size - bytes_recd, 2048))
-            if chunk == '':
+        data_buffer_all = []
+        num_bytes_recorded = 0
+        while num_bytes_recorded < self.buffer_size:
+            data_buffer_part = self.socket_obj.recv(self.buffer_size - num_bytes_recorded)
+            # data_buffer_part = self.socket_obj.recv(min(self.buffer_size - num_bytes_recorded, 2048))
+            if data_buffer_part == '':
                 raise RuntimeError("socket connection broken")
 
-            # for byteChar in chunk:
-            #     chunks.append("%02X" % ord(byteChar))
-            chunk = np.array([ord(x) for x in chunk])
-            # chunk = ''.join(["%02X " % ord(x) for x in chunk]).strip()
+            data_buffer_part = np.frombuffer(data_buffer_part, dtype=np.uint8)
+            # data_buffer_part = np.array([ord(x) for x in data_buffer_part], dtype=np.uint8)
 
-            chunks = np.append(chunks, chunk)
-            bytes_recd = bytes_recd + len(chunk)
+            data_buffer_all = np.append(data_buffer_all, data_buffer_part)
+            num_bytes_recorded = num_bytes_recorded + len(data_buffer_part)
 
-        return chunks
-        # return ''.join(chunks)
+        return data_buffer_all
+        # return ''.join(data_buffer_all)
 
 
-class Processing:
-    def __init__(self):
+class Processing(Filtering):
+    def __init__(self, hp_thresh, lp_thresh, notch_thresh):
+        Filtering.__init__(self, hp_thresh, lp_thresh, notch_thresh)
         self.data_channel = []
 
         self.__flag_start_bit = 165
@@ -86,7 +103,7 @@ class Processing:
 
         now = datetime.datetime.now()
         self.__saving_dir = "%d%02d%02d" % (now.year, now.month, now.day)
-        self.__saving_filename = "data%s%02d%02d%02d" % (self.__saving_dir, now.minute, now.second, now.microsecond)
+        self.__saving_filename = "data%s%02d%02d%02d%02d" % (self.__saving_dir, now.hour, now.minute, now.second, now.microsecond)
         self.__saving_full_filename = os.path.join(self.__saving_dir, self.__saving_filename) + ".csv"
 
         self.__create_saving_dir()  # create saving directory, skip if the file exists
@@ -95,62 +112,58 @@ class Processing:
     def demultiplex(self, data_orig):  # obtain complete samples and form a matrix ( data_channel )
         loc_start_orig = np.argwhere(data_orig == self.__flag_start_bit)
         loc_end_orig = np.argwhere(data_orig == self.__flag_end_bit)
-        # locs_start_orig = [i for i, x in enumerate(data_orig) if x == self.__flag_start]
-        # locs_end_orig = [i for i, x in enumerate(data_orig) if x == self.__flag_end]
 
-        loc_start = [x[0] for x in loc_start_orig if loc_end_orig[np.argmax(loc_end_orig > x)] - x == self.__sample_len]
-        # loc_start = [x for i, x in enumerate(loc_start_orig) if loc_end_orig > loc_start_orig]
+        loc_start = [x[0] for x in loc_start_orig
+                     if loc_end_orig[np.argmax(loc_end_orig > x)] - x == self.__sample_len]
 
-        data_all = [data_orig[x:x+self.__sample_len] for x in loc_start]
+        data_all = [data_orig[x:x + self.__sample_len+1] for x in loc_start]
         data_all = np.vstack(data_all)  # stack the arrays into one column
 
-        self.data_channel = data_all[:, 1:self.__sample_len-1]
+        self.data_channel = data_all
+        # self.data_channel = data_all[:, 1:self.__sample_len-1]
 
         return self.data_channel
+
+    def get_data_channel(self):
+        self.data_channel = np.ndarray(shape=(len(self.data_channel), 5), dtype='>u2',
+                                       buffer=np.array(self.data_channel, dtype=np.uint8).tobytes)
+
+    def save(self):  # save the data
+        np.savetxt(self.__saving_file_obj, self.data_channel, fmt="%d", delimiter=",")
+        # print("Saved file %s ..." % self.__saving_full_filename)
 
     def __create_saving_dir(self):
         if not os.path.exists(self.__saving_dir):
             os.makedirs(self.__saving_dir)
 
-    def save(self):  # save the data
-        np.savetxt(self.__saving_file_obj, self.data_channel, fmt="%d")
-        # print("Saved file %s ..." % self.__saving_full_filename)
-
 
 IP_ADD = "127.0.0.1"
 PORT = 8888
-BUFFER_SIZE = 25 * 66  # about 50 ms
+BUFFER_SIZE = 25 * 130  # about 50 ms
+
+HP_THRESH = 50
+LP_THRESH = 3500
+NOTCH_THRESH = 50
 
 if __name__ == "__main__":
     # setup and connect ot port
-    tcp_ip_obj = TcpIp()
-    tcp_ip_obj.set(IP_ADD, PORT, BUFFER_SIZE)
-    tcp_ip_obj.connect()
-    print("Connected...")
+    tcp_ip_obj = TcpIp(IP_ADD, PORT, BUFFER_SIZE)  # create port object
+    tcp_ip_obj.connect()  # connect to port
 
-    data = Processing()
-    print("Created data class...")
+    data_obj = Processing(HP_THRESH, LP_THRESH, NOTCH_THRESH)  # create data class
 
     count = 1
     while 1:
-        print("Loop %d:" % count)
+        # print("Loop %d:" % count)
 
         # read data
         data_orig = tcp_ip_obj.read()
-        # print("\ndata_orig: \n")
-        # print(data_orig)
-        #
-        # print("\ntype of element in data_orig: \n")
-        #
-        # print("Finished reading data...")
 
         # demultiplex and get the channel data
-        Processing.demultiplex(data, data_orig)
-        Processing.save(data)
-        # print(data_orig)
+        Processing.demultiplex(data_obj, data_orig)
+        # Processing.get_data_channel(data_obj)
+        Processing.save(data_obj)
 
         count += 1
-        # sleep(2)
 
     # print("Finished...")
-
