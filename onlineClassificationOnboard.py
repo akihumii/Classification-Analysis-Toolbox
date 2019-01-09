70ba681f50c7b6906901a91cbfadf0d6a93c2ba7
akihumii <a0126587@u.nus.edu>
2019-01-10 00:14:50 +0800

    using shihchiang's structure and then boom no more data drop LOL XD

--- onlineClassificationOnboard.py
+++ onlineClassificationOnboard.py
@@ -51,21 +51,15 @@ class Filtering:
 
 
 class TcpIp(threading.Thread):
-    def __init__(self, ip_add, port, buffer_size, buffer_obj):
+    def __init__(self, ip_add, port, buffer_size):
         threading.Thread.__init__(self)
         self.ip_add = ip_add
         self.port = port
 
         self.buffer_size = buffer_size
         self.socket_obj = None
-        self.buffer_obj = buffer_obj
 
         self.__connected = False
-
-    def run(self):  # for thread running
-        self.connect()
-        while True:
-            self.read()
 
     def connect(self):  # connect to port
         self.socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -82,50 +76,34 @@ class TcpIp(threading.Thread):
 
         print("Successfully connected...")
 
-    def read(self):  # read data from port
-        global ring_buffer
-        # if not self.buffer_obj.isSet():  # wait until demultiplexing finishes
+    def read(self, buffer_leftover):  # read data from port
         num_bytes_recorded = 0
-        data_buffer_all = []
+        buffer_read = np.array([], dtype=np.uint8)
         while num_bytes_recorded < self.buffer_size:
-            data_buffer_part = self.socket_obj.recv(self.buffer_size - num_bytes_recorded)
-            # data_buffer_part = self.socket_obj.recv(min(self.buffer_size - num_bytes_recorded, 2048))
-            if data_buffer_part == '':
+            buffer_part = self.socket_obj.recv(self.buffer_size - num_bytes_recorded)
+            if buffer_part == '':
                 raise RuntimeError("socket connection broken")
 
-            data_buffer_part = np.frombuffer(data_buffer_part, dtype=np.uint8)
-            # data_buffer_part = np.array([ord(x) for x in data_buffer_part], dtype=np.uint8)
+            buffer_read = np.append(buffer_read, np.frombuffer(buffer_part, dtype=np.uint8))
 
-            data_buffer_all = np.append(data_buffer_all, data_buffer_part)
-            # data_buffer_all = np.append(data_buffer_all, data_buffer_part)
-            num_bytes_recorded = num_bytes_recorded + len(data_buffer_part)
+            num_bytes_recorded = num_bytes_recorded + len(buffer_part)
 
-        self.buffer_obj.acquire()  # lock the mutex ring buffer
-        # print("lock in reading...")
-        for x in data_buffer_all:
-            ring_buffer.append(x)
-        self.buffer_obj.release()  # release the mutex ring buffer
-        # print("release in reading...")
-
-        # self.buffer_obj.set()  # stop it temporarily while waiting for demultiplexing process grabs the buffer
-        # print("In reading, threading event is %s" % self.buffer_obj.isSet())
-
-        # return ''.join(data_buffer_all)
+        return np.append(buffer_leftover, buffer_read)
 
 
 class Processing(threading.Thread):
-    def __init__(self, hp_thresh, lp_thresh, notch_thresh, buffer_obj):
+    def __init__(self, hp_thresh, lp_thresh, notch_thresh):
         threading.Thread.__init__(self)
         # Filtering.__init__(self, hp_thresh, lp_thresh, notch_thresh)
-        self.buffer_obj = buffer_obj
-
         self.data_orig = []
         self.data_channel = []
+        self.buffer_process = []
+        self.loc_start =[]
+        self.loc_start_orig = []
 
         self.__flag_start_bit = 165
         self.__flag_end_bit = 90
-        self.__sample_len = 24
-        self.__process_flag = False
+        self.__sample_len = 25
 
         now = datetime.datetime.now()
         self.__saving_dir = "%d%02d%02d" % (now.year, now.month, now.day)
@@ -135,62 +113,53 @@ class Processing(threading.Thread):
         self.__create_saving_dir()  # create saving directory, skip if the file exists
         self.__saving_file_obj = open(self.__saving_full_filename, "a")
 
-    def run(self):  # for thread running
-        global ring_buffer
-        while True:
-            self.get_buffer()
-            if self.__process_flag:
-                self.demultiplex()  # demultiplex and get the channel data
-                # Processing.get_data_channel(data_obj)
-                self.save()
+    def get_buffer(self, buffer_read):
+        self.loc_start_orig = np.argwhere(np.array(buffer_read) == self.__flag_start_bit)
 
-    def get_buffer(self):
-        self.buffer_obj.acquire()  # lock the mutex ring buffer
-        # print("lock in grabbing...")
-        global ring_buffer
-        # data_orig = np.array(ring_buffer)
-        # ring_buffer = RingBuffer(capacity=40980, dtype=np.uint8)
-        if len(ring_buffer) > 0:
-            # print("ring buffer size > 0...")
-            # print(len(ring_buffer))
-            self.data_orig = np.array([ring_buffer.popleft() for x in range(len(ring_buffer))])
-            self.__process_flag = True
-            # print(len(ring_buffer))
-        # else:
-            # print("ring buffer size == 0...")
-        self.buffer_obj.release()  # release the mutex ring buffer
-        # print("release in grabbing...")
+        self.loc_start = [x[0] for x in self.loc_start_orig
+                          if x + self.__sample_len < len(buffer_read)
+                          and buffer_read[x + self.__sample_len - 1] == self.__flag_end_bit]
+
+        [self.buffer_process, buffer_leftover] = np.split(buffer_read, [self.loc_start[-1]+self.__sample_len-1])
+
+        return buffer_leftover
 
     def demultiplex(self):  # obtain complete samples and form a matrix ( data_channel )
-        loc_start_orig = np.argwhere(self.data_orig == self.__flag_start_bit)
-        # loc_end_orig = np.argwhere(self.data_orig == self.__flag_end_bit)
-
-        loc_start = [x[0] for x in loc_start_orig
-                     if x+self.__sample_len <= len(self.data_orig)
-                     and self.data_orig[x+self.__sample_len] == self.data_orig[x-1]
-                     and self.data_orig[x-1] == self.__flag_end_bit]
-
-        # loc_start = [x[0] for x in loc_start_orig
-        #              if loc_end_orig[np.argmax(loc_end_orig > x)] - x == self.__sample_len]
-
-        data_all = [self.data_orig[x:x + self.__sample_len+1] for x in loc_start]
+        data_all = [self.buffer_process[x:x + self.__sample_len-1] for x in self.loc_start]
         data_all = np.vstack(data_all)  # stack the arrays into one column
 
-        self.data_channel = data_all[:, 1:self.__sample_len]
-        # self.data_channel = data_all[:, 1:self.__sample_len-1]
+        self.data_channel = data_all[:, 1:self.__sample_len-1]
 
     def get_data_channel(self):
         self.data_channel = np.ndarray(shape=(len(self.data_channel), 5), dtype='>u2',
                                        buffer=np.array(self.data_channel, dtype=np.uint8).tobytes)
 
-    def save(self):  # save the data
-        # np.savetxt(self.__saving_file_obj, self.data_orig, fmt="%d", delimiter=",")
-        np.savetxt(self.__saving_file_obj, self.data_channel, fmt="%d", delimiter=",")
-        # print("Saved file %s ..." % self.__saving_full_filename)
+    def save(self, data):  # save the data
+        np.savetxt(self.__saving_file_obj, data, fmt="%d", delimiter=",")
 
     def __create_saving_dir(self):
         if not os.path.exists(self.__saving_dir):
             os.makedirs(self.__saving_dir)
+
+
+class ReadNDemultiplex(threading.Thread):
+    def __init__(self, tcp_ip_obj, data_obj):
+        threading.Thread.__init__(self)
+        self.tcp_ip_obj = tcp_ip_obj
+        self.data_obj = data_obj
+        self.start()
+        self.join()
+
+    def run(self):
+        self.tcp_ip_obj.connect()
+        ring_buffer = []
+        # ring_buffer = RingBuffer(capacity=40960, dtype=np.uint8)
+        while True:
+            ring_buffer = self.tcp_ip_obj.read(ring_buffer)
+            ring_buffer = self.data_obj.get_buffer(ring_buffer)
+            self.data_obj.demultiplex()  # demultiplex and get the channel data
+            # self.data_obj.get_data_channel()
+            self.data_obj.save(data_obj.data_channel)
 
 
 IP_ADD = "127.0.0.1"
@@ -203,16 +172,11 @@ NOTCH_THRESH = 50
 
 if __name__ == "__main__":
     buffer_obj = threading.Lock()
-    ring_buffer = RingBuffer(capacity=40980, dtype=np.uint8)  # global variable
+    buffer_leftover = []
 
-    # setup and connect ot port
-    tcp_ip_obj = TcpIp(IP_ADD, PORT, BUFFER_SIZE, buffer_obj)  # create port object
-    data_obj = Processing(HP_THRESH, LP_THRESH, NOTCH_THRESH, buffer_obj)  # create data class
+    tcp_ip_obj = TcpIp(IP_ADD, PORT, BUFFER_SIZE)  # create port object
+    data_obj = Processing(HP_THRESH, LP_THRESH, NOTCH_THRESH)  # create data class
 
-    tcp_ip_obj.start()
-    data_obj.start()
-
-    tcp_ip_obj.join()
-    data_obj.join()
+    ReadNDemultiplex(tcp_ip_obj, data_obj)  # use one thread to do reading buffer and demultiplex
 
     # print("Finished...")
