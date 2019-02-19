d030d5ef88ebfff782115ddb601f337462ab85fa
akihumii <a0126587@u.nus.edu>
2019-02-19 18:05:23 +0800

    added another button to save features, save classes, train python classifier, send classifier to raspberry pi XD

--- Display/dispPredictionDialog.m
+++ Display/dispPredictionDialog.m
@@ -37,7 +37,7 @@ buttonStartStop = uicontrol(gcf,'Style','push','String','Start','FontWeight','bo
 
 buttonReselect = uicontrol(gcf,'Style','push','String','Reselect','FontWeight','bold','ForegroundColor','k','FontSize',textReselectSize,'Unit','normalized','Position',[.78,0.855,0.2,0.1],'CallBack',@reselectFile);
 buttonTrain = uicontrol(gcf,'Style','push','String','Train','FontWeight','bold','ForegroundColor','k','FontSize',textReselectSize,'Unit','normalized','Position',[.78,0.755,0.2,0.1],'CallBack',@trainClassifier);
-
+buttonSaveFeature = uicontrol(gcf,'Style','push','String','SaveFeatures','FontWeight','bold','ForegroundColor','k','FontSize',textReselectSize,'Unit','normalized','Position',[.78,0.655,0.2,0.1],'CallBack',@saveFeatures);
 
     function changeState(~,~)
         switch stopFlag
@@ -94,6 +94,71 @@ buttonTrain = uicontrol(gcf,'Style','push','String','Train','FontWeight','bold',
             popMsg('Training failed...');
         end
         drawnow
+    end
+
+    function saveFeatures(~,~)
+        disp(' ')
+        try
+            [threshMultStr, signal, signalClassificationInfo, saveFileName] = onlineClassifierDetectBursts();
+            saveBurstsInfo(signal, signalClassificationInfo, saveFileName);
+        catch
+        end
+        drawnow
+    end
+
+    function saveBurstsInfo(signal, signalClassificationInfo, saveFileName)
+        featuresForClassification = [5,7];  % selected features for classification        
+        numChannel = length(signalClassificationInfo);
+        
+        % Make a folder to store the features and corresponding classes
+        filepath = fileparts(saveFileName{1,1});
+        filepath = fullfile(filepath, 'classificationTmp');
+        if ~exist(filepath,'file')
+            mkdir(filepath);
+        end
+        
+        for i = 1:numChannel
+            timeStamps = time2string();
+            [~, filename] = fileparts(saveFileName{1,1});
+            fullfilenameFeature = fullfile(filepath,sprintf('featuresCh%d_%s_%s.csv', signal(i,1).channel(1,i), filename, timeStamps));
+            fullfilenameClass = fullfile(filepath,sprintf('classCh%d_%s_%s.csv', signal(i,1).channel(1,i), filename, timeStamps));
+            
+            featureStruct = signalClassificationInfo(i,1).features;
+            featureStruct = rmfield(featureStruct, 'dataAnalysed');  % to make it able to build a full table
+            featureTable = struct2table(featureStruct);
+            
+            feature = table2array(featureTable(:,featuresForClassification));  % get the target features
+            feature = feature(:,i:numChannel:end);  % get the target class
+            feature = reshape(feature,[],2);  % reshape into columns of different features
+            
+            feature = omitNan(feature,2,'any');
+            numBursts = size(feature,1);
+            class = reshape(repmat([1,2], numBursts/2, 1), [], 1);
+            
+            % save features and classes
+            csvwrite(fullfilenameFeature, feature)
+            disp(['Saved ', fullfilenameFeature, '...']);
+            csvwrite(fullfilenameClass, class)
+            disp(['Saved ', fullfilenameClass, '...']);
+        end
+        
+        popMsg('Features and Classes saving finished...');
+        
+        % train python classifier
+        systemCmd = sprintf('python %s %s', fullfile('C:', 'classificationTraining.py'), filepath);
+        system(systemCmd)
+        
+        % transfer classifier to rpi
+        cwd = pwd;
+        cd(filepath)
+        savedClassifier = dir('*.sav');  % get all the saved classifier
+        cd(cwd)
+        
+        for i = 1:length(savedClassifier)
+            systemCmd = sprintf('pscp -pw raspberry -scp %s pi@192.168.4.3:~/classificationTmp/', fullfile(filepath, savedClassifier(i,1).name));
+            system(systemCmd)
+        end
+
     end
 
     function resetAll()
--- onlineClassifierTraining.m
+++ onlineClassifierTraining.m
@@ -3,33 +3,11 @@ function [] = onlineClassifierTraining(varargin)
 %saveInfo for the online decoding
 %   [] = onlineClassifierTraining()
 
-% put 'warning('query','all')' in ClassificationECOC>localFitECOC/loopBody
+[threshMultStr, signal, signalClassificationInfo, saveFileName] = onlineClassifierDetectBursts();
 
-%% input threshold multiplier
-prompt = {'Channel 1:','Channel 2:','Channel 3:','Channel 4:'};
-title = 'Input threshold multiplier';
-dims = [1 35];
-% definput = {'190','180','95','60'};
-definput = {'30','15','30','15'};
-threshMultStr = inputdlg(prompt,title,dims,definput);
-threshMult = str2double(threshMultStr)';
-targetSubject = 'NHP'; % inupt either 'Derek' for biceps NRF demo set; or NHP for monkey device
+classifierOutput = analyzeFeatures('numFeaturesInCombination',2,'featureIndexSelected',{[5,7]},'selectFileType',2,'specificTarget',saveFileName,'showAccuracy',0,'saveAccuracy',0); % to train the classifier
 
-%% Pre-train
-switch targetSubject
-    case 'Derek'
-        [signal,signalClassificationInfo,saveFileName] = mainClassifier('threshStdMult',threshMult,'showOverlap',0,'saveOverlap',0); % to detect the bursts
-        
-    case 'NHP'
-        [signal,signalClassificationInfo,saveFileName] = mainClassifier('threshStdMult',threshMult,'showOverlap',0,'baselineType','movingWindow',...
-            'saveOverlap',0,'showRaw',0,'showFilt',0,'saveRaw',0,'saveFilt',0,'saveOverlap',0,...
-            'threshStdMult',[25,10,10,30],'TKEOStartConsecutivePoints',[25,45,45,45],'saveUserInput',1,'padZeroFlag',0,'burstTrimming',0); % to detect the bursts
-        
-    otherwise
-        error('Invalid input targetSubject...')
-end
 
-[classifierOutput] = analyzeFeatures('numFeaturesInCombination',2,'featureIndexSelected',{[5,7]},'selectFileType',2,'specificTarget',saveFileName,'showAccuracy',0,'saveAccuracy',0); % to train the classifier
 
 %% Save required information for online classification
 numClass = length(signal);
