967411d056235fa25976104272910a76f36dd025
akihumii <a0126587@u.nus.edu>
2019-04-03 20:44:54 +0800

    added in interpolation before doing the filtering, then only do stitching after that :O

--- PostProcessing/omitThresholdData.m
+++ PostProcessing/omitThresholdData.m
@@ -15,11 +15,10 @@ for i = 1:length(signal)
     for k = 1:numBursts
         deletingIndexCell{k,1} = startingPoint(k,1) : endPoint(k,1);
     end
-    deletingIndex(:,1) = reshape(cell2nanMat(deletingIndexCell(:,1)), [], 1);
         
     % delete the corresponding points
-    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'dataRaw', deletingIndex);
-    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'time', deletingIndex);
+    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'dataRaw', deletingIndexCell, 'interpolate');
+    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'time', deletingIndexCell, 'interpolate');
     
     signal(i,1) = rectifyData(signal(i,1),'dataRaw'); % rectify data
     signal(i,1) = filterData(signal(i,1),parameters); % filter data
@@ -28,8 +27,18 @@ for i = 1:length(signal)
         signal(i,1) = fftDataConvert(signal(i,1),parameters.dataToBeFFT); % do FFT
     end
     
+    % cut again after the filter artifact has been prevented
+    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'dataRaw', deletingIndexCell, parameters.stitchFlag);
+    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'time', deletingIndexCell, parameters.stitchFlag);
+    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'dataRectified', deletingIndexCell, parameters.stitchFlag);
+    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'dataFiltered', deletingIndexCell, parameters.stitchFlag);
+    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'dataTKEO', deletingIndexCell, parameters.stitchFlag);
+    signal(i,1) = deleteCorrespondingIndex(signal(i,1), 'dataFFT', deletingIndexCell, parameters.stitchFlag);
+    
     % update starting point
-    startingPointNew = updateStartingPoint(startingPoint, length(deletingIndexCell{1,1}));
+    if ~strcmp(parameters.stitchFlag, 'interpolate')
+        startingPoint = updateStartingPoint(startingPoint, length(deletingIndexCell{1,1}));
+    end
     
 end
 
@@ -41,7 +50,7 @@ end
 %% Output
 output = makeStruct(...
     signal,...
-    startingPointNew);
+    startingPoint);
 
 end
 
@@ -51,8 +60,9 @@ windowExtra = transpose(0 : windowSize : windowSize * (numBursts-1));
 dataNew = data - windowExtra;
 end
 
-function signal = deleteCorrespondingIndex(signal, dataName, deletingIndex)
+function signal = deleteCorrespondingIndex(signal, dataName, deletingIndex, stitchFlag)
 data = signal.(dataName);
+% get the data values
 if isstruct(signal.(dataName))
     data = data.values;
 end
@@ -60,16 +70,37 @@ if strcmp(dataName, 'time')
     data = data';
 end
 
+% delete data
 for i = 1:size(data,2)
     dataTemp{i,1} = data(:,i);
-    dataTemp{i,1}(deletingIndex(:,1)) = [];
+    if strcmp(stitchFlag, 'interpolate')
+        if ~strcmp(dataName, 'time')
+            numBursts = length(deletingIndex);
+            for j = 1:numBursts
+                lenBursts = length(deletingIndex{j,1});
+                startingValue = dataTemp{i,1}(deletingIndex{j,1}(1,1) - 1);
+                endValue = dataTemp{i,1}(deletingIndex{j,1}(1,end) + 1);
+                dataInterpolationTemp = linspace(startingValue, endValue, lenBursts);
+                dataTemp{i,1}(deletingIndex{j,1}) = dataInterpolationTemp;
+            end
+        end        
+    else
+        deletingIndexTemp = reshape(cell2nanMat(deletingIndex), [], 1);
+        dataTemp{i,1}(deletingIndexTemp) = [];
+    end
+    
+    if strcmp(stitchFlag, 'stitch') && strcmp(dataName, 'time')
+        dataTemp{i,1} = 1:size(dataTemp{i,1}, 1);
+    end
 end
 
+
+% replace the values in signal object
 if isstruct(signal.(dataName))
     signal.(dataName).values = cell2nanMat(dataTemp);
 elseif strcmp(dataName, 'time')
     signal.(dataName) = cell2nanMat(dataTemp)';
-else
+elseif ~isempty(data)
     signal.(dataName) = cell2nanMat(dataTemp);
 end
 end
