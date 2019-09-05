f8c7445477625eeb7bd1a0e7b700cf5ab67b8da2
akihumii <a0126587@u.nus.edu>
2019-09-05 12:32:07 +0800

    get fft of 3rd dimension of the input data :)

--- PreProcessing/fftDataConvert.m
+++ PreProcessing/fftDataConvert.m
@@ -5,17 +5,18 @@ function [dataFFT, fqDomain] = fftDataConvert(data, Fs)
 
 lengthData = size(data,1); % Length of data
 
-for j = 1:size(data,2)
-%     P3 = fft(data(:,j));
-%     P2 = abs(P3 / lengthData);
-%     P1 = P2(1 : floor(lengthData/2+1));
-%     P1(2 : end-1) = 2*P1(2 : end-1);
-    
-    P2 = fft(data(:,j));
-    P1 = abs(P2);
-    dataFFT(:,j) = P1;
+for n = 1:size(data,3)
+    for j = 1:size(data,2)
+        %     P3 = fft(data(:,j));
+        %     P2 = abs(P3 / lengthData);
+        %     P1 = P2(1 : floor(lengthData/2+1));
+        %     P1(2 : end-1) = 2*P1(2 : end-1);
+        
+        P2 = fft(data(:,j));
+        P1 = abs(P2);
+        dataFFT(:,j,n) = P1;
+    end
 end
-
 dF = Fs/lengthData;
 fqDomain = 0 : dF : Fs-dF;
 % fqDomain = Fs*(0:floor((lengthData/2)))/lengthData;
